e19cf7e2fea273dbdbde587f4a0fd0fd76415845
Tomasz Klim <github@tomaszklim.pl>
2018-09-03 16:01:43 +0200

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+sf-security is a proxy extension, responsible for installing
+the whole list of security-related extensions.
+
+By creating `/etc/local/.config/security.disable` file you can
+avoid installing these extensions. However you can still install
+them by individually.
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -s /etc/local/.config/security.disable ]; then
+	echo "skipping security configuration, forced by security.disable file"
+	exit 0
+fi
+
+/opt/farm/scripts/setup/extension.sh sf-secure-fs
+/opt/farm/scripts/setup/extension.sh sf-secure-sshd
+/opt/farm/scripts/setup/extension.sh sf-secure-kernel
+/opt/farm/scripts/setup/extension.sh sf-secure-system
+/opt/farm/scripts/setup/extension.sh sf-detect-suspicious
